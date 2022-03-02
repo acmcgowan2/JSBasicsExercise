5db9d07612d252a67307b0c2b553010ed68dd2a5
acmcgowan2”git config --global user.email “acmcgowan2@gmail.com” <student@Cumberlandss-MacBook-Pro-5.local>
2022-03-01 20:33:28 -0600

    added JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log(32);
+
+/*
+Strings
+Boolean
+Numbers
+Null
+Undefined
+*/
+
+// +
+// -
+// *
+// /
+// %
+// **
+
+console.log(5%4); // 1
+console.log(5%2); // 1
+
+console.log('I\'m Mary Poppins Y\'all');
+
+console.log("Hawk" + "eye");
+
+console.log("Adam".length +"McGowan".length);// 11
+
+console.log("Avengers: Endgame"[16]);
+
+// BONUS!!!
+
+console.log("\\"); // \
+
+console.log(5 ** 4 -9); //616
+
+console.log("I".length + "Am".length % "Iron".length - "Man".length); // 0
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+let m = 25;
+let c = 4;
+let u = m+c;
+console.log(u);
